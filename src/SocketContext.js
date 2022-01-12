171877aeea990a01d3333b4c7ab3371f255c982d
taauntik <devauntik@gmail.com>
2022-01-12 22:20:22 +0600

    done

--- src/SocketContext.js
+++ src/SocketContext.js
@@ -4,8 +4,8 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io("http://localhost:5000");
-// const socket = io("https://video-chat-application-backend.vercel.app/");
+// const socket = io("http://localhost:5000");
+const socket = io("https://video-chat-application-backend.vercel.app/");
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
